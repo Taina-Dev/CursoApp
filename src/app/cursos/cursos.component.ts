8523585e4563c459904589159c88a21923f33abd
taina <taina-correa195@hotmail.com>
2021-11-16 22:59:17 -0300

    quase la

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -2,6 +2,8 @@ import { Curso } from './../shared/cursos.model';
 import { CursosService } from './../shared/cursos.service';
 import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { NgForm } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
@@ -39,6 +41,42 @@ export class CursosComponent implements OnInit {
       )
     }
   }
+  onSubmit(form: NgForm) {
+    if(this.service.curso == null ) return
+
+    if (this.service.curso.cursoId == 0)
+      this.insertRecord(form);
+    else
+      this.updateRecord(form);
+  }
+  insertRecord(form: NgForm) {
+    if(this.service.curso == null ) return
+    this.service.postCurso().subscribe(
+      res => {
+        this.resetForm(form);
+        this.service.refreshList();
+        this.toastr.success('Inserido com sucesso', 'resgistro de')
+      },
+      err => { console.log(err); }
+    );
+  }
+  updateRecord(form: NgForm) {
+    this.service.putCurso().subscribe(
+      res => {
+        this.resetForm(form);
+        this.service.refreshList();
+        this.toastr.info('Salvo com sucesso', 'registro de curso')
+      },
+      err => { console.log(err); }
+    );
+  }
+  resetForm(form: NgForm) {
+    form.form.reset();
+
+  }
 
 
 }
+
+
+
